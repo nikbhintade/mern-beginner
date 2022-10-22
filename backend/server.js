2dba510409c0a12220a977bfbcf0f0b76005476c
Nikhil <29760727+nikbhintade@users.noreply.github.com>
2022-10-22 17:51:53 +0530

    health endpoint

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,33 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// CORS
 
-app.use("/api/goals", require("./routes/goalRoutes"))
-app.use("/api/users", require("./routes/userRoutes"))
+// app.use((req, res ,next) =>
+// {
+//     res.header("Access-Control-Allow-Origin","*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+//         );
+//     if (req.method === 'OPTIONS')
+//     {
+//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+//         return res.status(200).json({});
+//     }
+
+//   // Pass next middleware
+//   next();    
+
+// });
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.get("/health", (req, res) => {
+    console.log("GET endpoint hit");
+    res.status(200).send('Ok');
+    console.log("Status should be returned")
+});
 
 // NOTE: error middleware needs to be below the routes only then defualt error handler is overidden
 app.use(errorHandler);
